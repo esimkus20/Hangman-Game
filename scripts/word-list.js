07aee7589b7d0c0f93913ea15081a87f7874f187
Ernestas <55585785+esimkus20@users.noreply.github.com>
2023-09-03 01:31:26 +0300

    Add files via upload

---
+++ scripts/word-list.js
@@ -0,0 +1,82 @@
+const wordList = [
+  {
+    word: "apple",
+    hint: "A round fruit with crisp flesh and a red or green skin."
+  },
+  {
+    word: "elephant",
+    hint: "A large, herbivorous mammal with a trunk and long tusks."
+  },
+  {
+    word: "computer",
+    hint: "An electronic device for processing and storing data."
+  },
+  {
+    word: "oxygen",
+    hint: "A chemical element essential for life and respiration."
+  },
+  {
+    word: "guitar",
+    hint: "A musical instrument with strings that is played by strumming."
+  },
+  {
+    word: "butterfly",
+    hint: "An insect with colorful wings that undergoes metamorphosis."
+  },
+  {
+    word: "library",
+    hint: "A place where books and other materials are available for reading."
+  },
+  {
+    word: "piano",
+    hint: "A musical instrument with keys that produce musical notes."
+  },
+  {
+    word: "rainbow",
+    hint: "A meteorological phenomenon with colors in the sky after rain."
+  },
+  {
+    word: "volcano",
+    hint: "A mountain that erupts with hot lava, ash, and gases."
+  },
+  {
+    word: "beach",
+    hint: "A sandy shore by the ocean or other body of water."
+  },
+  {
+    word: "bicycle",
+    hint: "A human-powered vehicle with two wheels."
+  },
+  {
+    word: "camera",
+    hint: "A device used to capture photographs or record videos."
+  },
+  {
+    word: "cloud",
+    hint: "A visible mass of water droplets or ice crystals in the sky."
+  },
+  {
+    word: "moon",
+    hint: "The natural satellite of Earth that orbits the planet."
+  },
+  {
+    word: "river",
+    hint: "A natural flowing watercourse, typically with freshwater."
+  },
+  {
+    word: "telephone",
+    hint: "A communication device used to make voice calls."
+  },
+  {
+    word: "garden",
+    hint: "An area where plants, flowers, or vegetables are grown."
+  },
+  {
+    word: "lion",
+    hint: "A large, carnivorous feline animal known for its mane."
+  },
+  {
+    word: "pizza",
+    hint: "A popular Italian dish consisting of a round flatbread topped with various ingredients."
+  }
+]
